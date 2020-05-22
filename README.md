e0ecc76a02997fec3e91d51efee88fa9e0776f1e
Vasiliy Makarov <drmoriarty.0@gmail.com>
2020-05-22 18:58:38 +0300

    v 0.0.1, for godot 3.2.2.beta

---
+++ README.md
@@ -0,0 +1,11 @@
+# godot-tenjin
+
+Tenjin plugin for godot engine. You need godot version 3.2.2.beta or higher.
+
+## Android usage
+
+Copy `Tenjin.gdap` to `android/plugins`. Then enable plugin `Tenjin` in export settings.
+
+## iOS usage
+
+Coming soon...
---
+++ android-plugin/src/main/java/ru/mobilap/tenjin/Tenjin.java
@@ -0,0 +1,128 @@
+package ru.mobilap.tenjin;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import com.facebook.applinks.AppLinkData;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient.Info;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.tenjin.android.Callback;
+import com.tenjin.android.TenjinSDK;
+import java.io.IOException;
+import java.lang.Exception;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.godotengine.godot.Godot;
+import org.godotengine.godot.GodotLib;
+import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
+
+public class Tenjin extends GodotPlugin
+{
+	private Activity activity = null;
+    private String apiKey = null;
+
+    public Tenjin(Godot godot) 
+    {
+        super(godot);
+        activity = godot;
+    }
+
+    @Override
+    public String getPluginName() {
+        return "Tenjin";
+    }
+
+    @Override
+    public List<String> getPluginMethods() {
+        return Arrays.asList(
+                             "init", "logEvent", "logEventWithValue", "logPurchase", "logPurchaseWithSignature", "advertising_id"
+                             );
+    }
+
+    /*
+    @Override
+    public Set<SignalInfo> getPluginSignals() {
+        return Collections.singleton(loggedInSignal);
+    }
+    */
+
+    @Override
+    public View onMainCreateView(Activity activity) {
+        return null;
+    }
+
+    public void init(final String _apiKey, final String _deepLinkUri) {
+        apiKey = _apiKey;
+        TenjinSDK instance = TenjinSDK.getInstance(activity, apiKey);
+        if(_deepLinkUri != null && _deepLinkUri != "") {
+            instance.connect(_deepLinkUri);
+        } else {
+            instance.connect();
+        }
+        Log.i("godot", "Tenjin plugin inited!");
+        Log.i("godot", "Device GAID: "+advertising_id());
+        instance.getDeeplink(new Callback() {
+            @Override
+            public void onSuccess(boolean clickedTenjinLink, boolean isFirstSession, Map<String, String> data) {
+                if (clickedTenjinLink) {
+                    if (isFirstSession) {
+                        if (data.containsKey(TenjinSDK.DEEPLINK_URL)) {
+                           // use the deferred_deeplink_url to direct the user to a specific part of your app
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public void logEvent(final String event) {
+        if(apiKey != null)
+            tenjinInstance().eventWithName(event);
+    }
+
+    public void logEventWithValue(final String event, final int value) {
+        if(apiKey != null)
+            tenjinInstance().eventWithNameAndValue(event, ""+value);
+    }
+
+    public void logPurchase(final String productId, final String currencyCode, final int quantity, final double unitPrice) {
+        if(apiKey != null)
+            tenjinInstance().transaction(productId, currencyCode, quantity, unitPrice);
+    }
+
+    public void logPurchaseWithSignature(final String productId, final String currencyCode, final int quantity, final double unitPrice, final String purchaseData, final String dataSignature) {
+        if(apiKey != null)
+            tenjinInstance().transaction(productId, currencyCode, quantity, unitPrice, purchaseData, dataSignature);
+    }
+    
+    public String advertising_id()
+    {
+        Info adInfo = null;
+        try {
+            adInfo = AdvertisingIdClient.getAdvertisingIdInfo(activity);
+            String userId = adInfo.getId();
+            return userId;
+        } catch (IOException e) {
+            Log.e("godot", e.toString());
+        //} catch (GooglePlayServicesAvailabilityException e) {
+            //Log.e("godot", e.toString());
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Log.e("godot", e.toString());
+        } catch (Exception e) {
+            Log.e("godot", e.toString());
+        }
+        return "";
+    }
+
+    private TenjinSDK tenjinInstance() {
+        return TenjinSDK.getInstance(activity, apiKey);
+    }
+
+    @Override public void onMainResume() {
+    } 
+
+}
